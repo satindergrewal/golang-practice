f9f3970b49685064bafc7dc22f6bc4ae5d99040c
Satinder Grewal <grewal@lavabit.com>
2018-11-10 04:26:48 +1300

    appdata dir test

--- src/goauth/temp.go
+++ src/goauth/temp.go
@@ -2,10 +2,14 @@ package main
 
 import "strings"
 import "fmt"
+import "goauth/btcutil"
 
 func main() {
     initial := "<h1>Hello World!</h1>"
 
     out := strings.TrimLeft(strings.TrimRight(initial,"</h1>"),"<h1>")
     fmt.Println(out)
+
+    dir := btcutil.AppDataDir("komodo", false)
+    fmt.Println(dir)
 }
