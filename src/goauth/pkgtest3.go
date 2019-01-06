04a746a82f891ac63f3646b5b6246bd1717673f5
Satinder Grewal <grewal@lavabit.com>
2019-01-07 08:06:21 +1300

    update

--- src/goauth/pkgtest3.go
+++ src/goauth/pkgtest3.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
-	"goauth/mypkg2"
+    "github.com/satindergrewal/kmdgo"
 )
 
 func main() {
-	var rval mypkg.GetInfo
-    rval = mypkg.ResultGetInfo()
-    //fmt.Println(rval)
-    //fmt.Println(rval.Result)
+    // Define appname variable. The name value must be the matching value of it's data directory name.
+    // Example Komodo's data directory is `komodo`, VerusCoin's data directory is `VRSC` and so on.
+    appName := `komodo`
+
+    // define the variable with GetInfo struct from pacakge kmdgo
+	var rval kmdgo.GetInfo
+
+    // Get output of the ResultGetinfo function and store it to GetInfo struct variable
+    rval = kmdgo.ResultGetInfo(appName)
+
+    // Can print and use the struct variable outputs in further code logic. Check GetInfo struct in package file.
+    fmt.Println(rval)
+    fmt.Println(rval.Result)
+    
     fmt.Println("Version:", rval.Result.Version)
     fmt.Println("Balance:", rval.Result.Balance)
     fmt.Println("Blocks:", rval.Result.Blocks)
@@ -17,6 +27,6 @@ func main() {
     fmt.Println("Connections:", rval.Result.Connections)
     fmt.Println("Difficulty:", rval.Result.Difficulty)
     fmt.Println("Magic:", rval.Result.Magic)
-    //fmt.Println(rval.Error)
-    //fmt.Println(rval.ID)
+    fmt.Println(rval.Error)
+    fmt.Println(rval.ID)
 }
---
+++ src/practice/temp.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 33
+	fmt.Println(&a)
+}
