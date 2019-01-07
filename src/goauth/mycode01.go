b992d66fbebb910fae680e9c85ba88ce74d398f5
Satinder Grewal <grewal@lavabit.com>
2019-01-08 09:13:44 +1300

    common API call method

---
+++ src/goauth/mycode01.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "log"
+    "bytes"
+    "encoding/json"
+    "github.com/satindergrewal/kmdgo/kmdutil"
+)
+
+type appType string
+
+type GetBestBlockhash struct {
+    Result string      `json:"result"`
+    Error  interface{} `json:"error"`
+    ID     string      `json:"id"`
+}
+
+type APIQuery struct {
+    Method string `json:"method"`
+    Params string `json:"params"`
+}
+
+
+func (appName appType) APICall(q APIQuery) string {
+    rpcuser, rpcpass, rpcport := kmdutil.AppRPCInfo(string(appName))
+
+    client := &http.Client{}
+    url := `http://127.0.0.1:`+rpcport
+
+    //fmt.Println(rpcuser)
+    //fmt.Println(rpcpass)
+    //fmt.Println(rpcport)
+    //fmt.Printf("%s\n", url)
+    //fmt.Println(q.Method)
+    //fmt.Println(q.Params)
+
+    var query_str string
+    query_str = `{"jsonrpc": "1.0", "id":"kmdgo", "method": "`+q.Method+`", "params": `+q.Params+` }`
+    //fmt.Println(query_str)
+
+    query_byte := []byte(query_str)
+    //fmt.Println(query_byte)
+
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(query_byte))
+    req.Header.Set("Content-Type", "application/json")
+
+    //req, err := http.NewRequest("POST", , nil)
+    req.SetBasicAuth(rpcuser, rpcpass)
+    resp, err := client.Do(req)
+    if err != nil{
+        log.Fatal(err)
+    }
+    bodyText, err := ioutil.ReadAll(resp.Body)
+
+    var query_result map[string]interface{}
+    if err := json.Unmarshal(bodyText, &query_result); err != nil {
+        panic(err)
+    }
+
+    s := string(bodyText)
+    return s
+}
+
+
+func GetBestBlockhashJsonValue(appName appType) string {
+    rpcuser, rpcpass, rpcport := kmdutil.AppRPCInfo(string(appName))
+
+    client := &http.Client{}
+    url := `http://127.0.0.1:`+rpcport
+
+    query_byte := []byte(`{"jsonrpc": "1.0", "id":"kmdgo", "method": "getbestblockhash", "params": [] }`)
+
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(query_byte))
+    req.Header.Set("Content-Type", "application/json")
+
+    //req, err := http.NewRequest("POST", , nil)
+    req.SetBasicAuth(rpcuser, rpcpass)
+    resp, err := client.Do(req)
+    if err != nil{
+        log.Fatal(err)
+    }
+    bodyText, err := ioutil.ReadAll(resp.Body)
+
+    var query_result map[string]interface{}
+    if err := json.Unmarshal(bodyText, &query_result); err != nil {
+        panic(err)
+    }
+
+    s := string(bodyText)
+    return s
+}
+
+func (appName appType) GetBestBlockhash() GetBestBlockhash {
+    query := APIQuery {
+        Method:     "getbestblockhash",
+        Params:   "[]",
+    }
+
+    //getbestblockhashJson := GetBestBlockhashJsonValue(appName)
+    getbestblockhashJson := appName.APICall(query)
+    var getbestblockhash GetBestBlockhash
+    json.Unmarshal([]byte(getbestblockhashJson), &getbestblockhash)
+    return getbestblockhash
+}
+
+func main() {
+    var appName appType
+    appName = `komodo`
+    //fmt.Printf("%T\n", appName)
+    var bh GetBestBlockhash
+    bh = appName.GetBestBlockhash()
+    fmt.Println(bh)
+    fmt.Println(bh.Result)
+    fmt.Println(bh.Error)
+    fmt.Println(bh.ID)
+
+    /*query := APIQuery {
+        Method:     "getbestblockhash",
+        Params:   "[]",
+    }
+
+    bh := appName.APICall(query)
+    fmt.Println(bh)*/
+}
