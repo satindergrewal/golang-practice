c33d95036d9ae27cd28ce832825a0ffbeb2b4361
Satinder Grewal <grewal@lavabit.com>
2019-01-10 15:34:45 +1300

    changes to error handling in methods

--- src/goauth/mycode02.go
+++ src/goauth/mycode02.go
@@ -20,40 +20,44 @@ type GetBestBlockhash struct {
 }
 
 type GetInfo struct {
-    Version             int     `json:"version"`
-    Protocolversion     int     `json:"protocolversion"`
-    KMDversion          string  `json:"KMDversion"`
-    Notarized           int     `json:"notarized"`
-    PrevMoMheight       int     `json:"prevMoMheight"`
-    Notarizedhash       string  `json:"notarizedhash"`
-    Notarizedtxid       string  `json:"notarizedtxid"`
-    NotarizedtxidHeight string  `json:"notarizedtxid_height"`
-    KMDnotarizedHeight  int     `json:"KMDnotarized_height"`
-    NotarizedConfirms   int     `json:"notarized_confirms"`
-    Walletversion       int     `json:"walletversion"`
-    Balance             float64 `json:"balance"`
-    Blocks              int     `json:"blocks"`
-    Longestchain        int     `json:"longestchain"`
-    Timeoffset          int     `json:"timeoffset"`
-    Tiptime             int     `json:"tiptime"`
-    Connections         int     `json:"connections"`
-    Proxy               string  `json:"proxy"`
-    Difficulty          float64 `json:"difficulty"`
-    Testnet             bool    `json:"testnet"`
-    Keypoololdest       int     `json:"keypoololdest"`
-    Keypoolsize         int     `json:"keypoolsize"`
-    Paytxfee            float64 `json:"paytxfee"`
-    Relayfee            float64 `json:"relayfee"`
-    Errors              string  `json:"errors"`
-    CCid                int     `json:"CCid"`
-    Name                string  `json:"name"`
-    P2Pport             int     `json:"p2pport"`
-    Rpcport             int     `json:"rpcport"`
-    Magic               int     `json:"magic"`
-    Premine             int     `json:"premine"`
-    Reward              int64   `json:"reward"`
-    Halving             int     `json:"halving"`
-    Commission          int     `json:"commission"`
+    Result struct {
+        Version             int     `json:"version"`
+        Protocolversion     int     `json:"protocolversion"`
+        KMDversion          string  `json:"KMDversion"`
+        Notarized           int     `json:"notarized"`
+        PrevMoMheight       int     `json:"prevMoMheight"`
+        Notarizedhash       string  `json:"notarizedhash"`
+        Notarizedtxid       string  `json:"notarizedtxid"`
+        NotarizedtxidHeight string  `json:"notarizedtxid_height"`
+        KMDnotarizedHeight  int     `json:"KMDnotarized_height"`
+        NotarizedConfirms   int     `json:"notarized_confirms"`
+        Walletversion       int     `json:"walletversion"`
+        Balance             float64 `json:"balance"`
+        Blocks              int     `json:"blocks"`
+        Longestchain        int     `json:"longestchain"`
+        Timeoffset          int     `json:"timeoffset"`
+        Tiptime             int     `json:"tiptime"`
+        Connections         int     `json:"connections"`
+        Proxy               string  `json:"proxy"`
+        Difficulty          float64 `json:"difficulty"`
+        Testnet             bool    `json:"testnet"`
+        Keypoololdest       int     `json:"keypoololdest"`
+        Keypoolsize         int     `json:"keypoolsize"`
+        Paytxfee            float64 `json:"paytxfee"`
+        Relayfee            float64 `json:"relayfee"`
+        Errors              string  `json:"errors"`
+        CCid                int     `json:"CCid"`
+        Name                string  `json:"name"`
+        P2Pport             int     `json:"p2pport"`
+        Rpcport             int     `json:"rpcport"`
+        Magic               int     `json:"magic"`
+        Premine             int     `json:"premine"`
+        Reward              int64   `json:"reward"`
+        Halving             int     `json:"halving"`
+        Commission          int     `json:"commission"`
+    } `json:"result"`
+    Error Error `json:"error"`
+    ID    string      `json:"id"`
 }
 
 type APIResult struct {
@@ -105,8 +109,8 @@ func (appName appType) APICall(q APIQuery) string {
 
 func (appName appType) GetBestBlockhash() (GetBestBlockhash, error) {
     query := APIQuery {
-        Method:     "getbestblockhash",
-        Params:   "[v]",
+        Method:     `getbestblockhash`,
+        Params:   `[]`,
     }
 
     var getbestblockhash GetBestBlockhash
@@ -115,14 +119,17 @@ func (appName appType) GetBestBlockhash() (GetBestBlockhash, error) {
     fmt.Println(getbestblockhashJson)
     
     
-    json.Unmarshal([]byte(getbestblockhashJson), &getbestblockhash)
+    var result APIResult
+
+    json.Unmarshal([]byte(getbestblockhashJson), &result)
     
-    if getbestblockhash.Result == nil {
+    if result.Result == nil {
         fmt.Println("!!! Return Error !!!")
-        answer_error, err := json.Marshal(getbestblockhash.Error)
+        answer_error, err := json.Marshal(result.Error)
         if err != nil {
             fmt.Println("error:", err)
         }
+        json.Unmarshal([]byte(getbestblockhashJson), &getbestblockhash)
         return getbestblockhash, errors.New(string(answer_error))
     }
 
@@ -130,26 +137,29 @@ func (appName appType) GetBestBlockhash() (GetBestBlockhash, error) {
     return getbestblockhash, nil
 }
 
-func (appName appType) GetInfo() (APIResult, error) {
+func (appName appType) GetInfo() (GetInfo, error) {
     query := APIQuery {
         Method:     "getinfo",
         Params:   "[]",
     }
 
-    var getinfo APIResult
+    var getinfo GetInfo
 
     getinfoJson := appName.APICall(query)
     fmt.Println(getinfoJson)
     
     
-    json.Unmarshal([]byte(getinfoJson), &getinfo)
+    var result APIResult
+
+    json.Unmarshal([]byte(getinfoJson), &result)
     
-    if getinfo.Error != nil {
+    if result.Result == nil {
         fmt.Println("!!! Return Error !!!")
-        answer_error, err := json.Marshal(getinfo.Error)
+        answer_error, err := json.Marshal(result.Error)
         if err != nil {
             fmt.Println("error:", err)
         }
+        json.Unmarshal([]byte(getinfoJson), &getinfo)
         return getinfo, errors.New(string(answer_error))
     }
 
@@ -160,9 +170,11 @@ func (appName appType) GetInfo() (APIResult, error) {
 func main() {
     var appName appType
     appName = `komodo`
+    var err error
     fmt.Printf("\n")
-    /*var bh GetBestBlockhash
-    bh, err := appName.GetBestBlockhash()
+
+    var bh GetBestBlockhash
+    bh, err = appName.GetBestBlockhash()
     if err != nil {
         log.Println("err happened", err)
     }
@@ -170,19 +182,22 @@ func main() {
     fmt.Println(bh.Result)
     fmt.Println(bh.Error.Code)
     fmt.Println(bh.Error.Message)
-    fmt.Println(bh.ID)*/
+    fmt.Println(bh.ID)
 
-    var res APIResult
+    /*var info GetInfo
 
-    res, err := appName.GetInfo()
+    info, err = appName.GetInfo()
     if err != nil {
         log.Println("err happened", err)
     }
-    fmt.Println(res.Result)
+    fmt.Println("getinfo value", info)
+    fmt.Println(info.Result)
+    fmt.Println(info.Error.Code)
+    fmt.Println(info.Error.Message)*/
 
     fmt.Printf("\n\n\n")
 
-    fmt.Printf("%T\n", res.Result)
+    //fmt.Printf("%T\n", info)
 
     //var gi GetInfo
     //gi = res.Result
