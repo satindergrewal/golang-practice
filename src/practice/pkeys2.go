968addc2165cd0aa701fefb3f0ea97b7e24fc763
Satinder Grewal <grewal@lavabit.com>
2019-01-31 02:19:36 +1300

    address generation test

---
+++ src/practice/pkeys2.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	//"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+type Network struct {
+	name        string
+	symbol      string
+	xpubkey     byte
+	xprivatekey byte
+}
+
+var network = map[string]Network{
+	"rdd": {name: "reddcoin", symbol: "rdd", xpubkey: 0x3d, xprivatekey: 0xbd},
+	"dgb": {name: "digibyte", symbol: "dgb", xpubkey: 0x1e, xprivatekey: 0x80},
+	"btc": {name: "bitcoin", symbol: "btc", xpubkey: 0x00, xprivatekey: 0x80},
+	"ltc": {name: "litecoin", symbol: "ltc", xpubkey: 0x30, xprivatekey: 0xb0},
+	"kmd": {name: "Komodo", symbol: "kmd", xpubkey: 0x3C, xprivatekey: 0xb4},
+}
+
+func (network Network) GetNetworkParams() *chaincfg.Params {
+	networkParams := &chaincfg.MainNetParams
+	networkParams.PubKeyHashAddrID = network.xpubkey
+	networkParams.PrivateKeyID = network.xprivatekey
+	return networkParams
+}
+
+func (network Network) CreatePrivateKey() (*btcutil.WIF, error) {
+	secret, err := btcec.NewPrivateKey(btcec.S256())
+	if err != nil {
+		return nil, err
+	}
+	//fmt.Println("secret: ", secret)
+	return btcutil.NewWIF(secret, network.GetNetworkParams(), false)
+}
+
+
+//func (network Network) ImportPrivateKey(secretHex string) (*btcutil.WIF, error) { }
+
+func (network Network) ImportWIF(wifStr string) (*btcutil.WIF, error) {
+	wif, err := btcutil.DecodeWIF(wifStr)
+	if err != nil {
+		return nil, err
+	}
+	//fmt.Println(*wif)
+	if !wif.IsForNet(network.GetNetworkParams()) {
+		return nil, errors.New("The WIF string is not valid for the `" + network.name + "` network")
+	}
+	return wif, nil
+}
+
+func (network Network) GetAddress(wif *btcutil.WIF) (*btcutil.AddressPubKey, error) {
+	return btcutil.NewAddressPubKey(wif.PrivKey.PubKey().SerializeCompressed(), network.GetNetworkParams())
+}
+
+func main() {
+    fmt.Println("Starting the application...")
+    wif, _ := network["kmd"].CreatePrivateKey()
+    fmt.Println(*wif)
+    address, _ := network["kmd"].GetAddress(wif)
+    fmt.Printf("%s - %s\n", wif.String(), address.EncodeAddress())
+
+    /*dwif, _ := network["kmd"].ImportWIF(wif.String())
+    fmt.Println("dwif: ", dwif)*/
+}
