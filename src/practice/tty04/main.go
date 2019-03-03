bab356555da49dbba02a68e0d8ad7c44b4ed3679
Satinder Grewal <grewal@lavabit.com>
2019-03-03 20:46:56 +1300

    Comments update in tty04 test file, middleware examples code

--- src/practice/tty04/main.go
+++ src/practice/tty04/main.go
@@ -29,12 +29,15 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
+
+// GoTTY command and argument struct
 type ttycommand struct {
 	Command   string
 	Arguments string
 	//Subscribed bool
 }
 
+// Gorilla Websocket upgrader settings
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -44,6 +47,7 @@ var upgrader = websocket.Upgrader{
 }
 
 
+// Some example user struct which is used with websocket url v3/ws
 type myStruct struct {
 	Username  string `json:"username"`
 	FirstName string `json:"firstName"`
@@ -52,35 +56,36 @@ type myStruct struct {
 
 func main() {
 
-	appName := "ROGUE"
-	dir := kmdutil.AppDataDir(appName, false)
-	fmt.Println(dir, "\n")
 
-	err := os.Chdir(dir)
+	appName := "ROGUE" //Name of my app
+	dir := kmdutil.AppDataDir(appName, false) //Based on the name of my app, it gets the location of this app on the machine.
+	fmt.Println(dir, "\n") //Displays the Directory location of the app
+
+	err := os.Chdir(dir) //Just changing the current working directory location to the directory location got from the AppDataDir output
 	if err != nil {
 		fmt.Println(err)
 	}
 	d, _ := os.Getwd()
-	fmt.Println(d)
+	fmt.Println(d) //Printing out the current working directory to console
 
 	var cat hotcat
 	cat = 1
 	mux := http.NewServeMux()
 	mux.Handle("/cat", cat)
 
-	http.HandleFunc("/", foo)
-	http.HandleFunc("/tty", tty)
+	http.HandleFunc("/", foo) //Displays the template form with input form to input command and argument
+	http.HandleFunc("/tty", tty) //Output of foo forwards to this, an iFrame page showing the URL http://localhost:8082, which is running a TTY cosonle app, example a console Rogue game.
 	//http.Handle("/cat", cat)
-	http.HandleFunc("/sockets", socketshandle)
-	http.HandleFunc("/v3/ws", v3ws)
+	http.HandleFunc("/sockets", socketshandle) //Just the usual example of gorilla websockets, Read and Write the output coming from browser websocket
+	http.HandleFunc("/v3/ws", v3ws) // Just another test example of gorilla websocket for testing.
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8081", mux)
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
 
-	c := req.FormValue("cmd")
-	a := req.FormValue("args")
+	c := req.FormValue("cmd") //Getting the command to pass to GoTTY to start the web SSH with.
+	a := req.FormValue("args") //Arguments for that command
 
 	err := tpl.ExecuteTemplate(w, "index.gohtml", ttycommand{c, a})
 	if err != nil {
@@ -93,15 +98,15 @@ func tty(w http.ResponseWriter, req *http.Request) {
 
 	//v := req.FormValue("q")
 	//fmt.Fprintln(w, "Do my search: "+v)
-	c := req.FormValue("cmd")
-	a := req.FormValue("args")
+	c := req.FormValue("cmd") //Getting the command from foo
+	a := req.FormValue("args") //Getting the command arguments from foo
 	fmt.Println(c)
 	//fmt.Println(a)
 
 	// visit this page:
 	// http://localhost:8080/tty?q=dog
 
-	appOptions := &server.Options{}
+	appOptions := &server.Options{} //Getting default GoTTY server options
 	if err := utils.ApplyDefaultValues(appOptions); err != nil {
 		exit(err, 1)
 	}
@@ -122,7 +127,7 @@ func tty(w http.ResponseWriter, req *http.Request) {
 	cmd_args := strings.Fields(a)
 	fmt.Printf("%T\n", cmd_args)
 
-	factory, err := localcommand.NewFactory(cmd, cmd_args, backendOptions)
+	factory, err := localcommand.NewFactory(cmd, cmd_args, backendOptions) //Passing the command and it's args to GoTTY
 	if err != nil {
 		exit(err, 3)
 	}
@@ -132,15 +137,15 @@ func tty(w http.ResponseWriter, req *http.Request) {
 
 	hostname, _ := os.Hostname()
 	appOptions.Port = `8082`
-	appOptions.PermitWrite = true
-	appOptions.Once = true
+	appOptions.PermitWrite = true //Setting GoTTY's permissions to accept input on web shell, so that Game can be played by user keystrokes on Web
+	appOptions.Once = true //Quit the GoTTY after the ROGUE game ends. The http://localhost:8082 will dead/exited as per this option set to true.
 	appOptions.TitleVariables = map[string]interface{}{
 		"command":  cmd,
 		"argv":     cmd_args,
 		"hostname": hostname,
 	}
 
-	srv, err := server.New(factory, appOptions)
+	srv, err := server.New(factory, appOptions) //Set GoTTY New ServeHTTP
 	if err != nil {
 		exit(err, 3)
 	}
@@ -157,15 +162,16 @@ func tty(w http.ResponseWriter, req *http.Request) {
 
 	errs := make(chan error, 1)
 	go func() {
-		errs <- srv.Run(ctx, server.WithGracefullContext(gCtx))
+		errs <- srv.Run(ctx, server.WithGracefullContext(gCtx)) //Execute GoTTY Server command and get it's context channel updates in errs
 	}()
 
-	err = tpl.ExecuteTemplate(w, "play.gohtml", nil)
+	err = tpl.ExecuteTemplate(w, "play.gohtml", nil) //the go html template page which has iframe pointing to http://localhost:8082
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		log.Fatalln(err)
 	}
 
+	// Once the srv.Run is done, execute this part of the code to close any gorotunes of GoTTY
 	go func() {
 		for {
 			err := <-errs
@@ -176,6 +182,7 @@ func tty(w http.ResponseWriter, req *http.Request) {
 			fmt.Printf("context type after Game end:\t%T\n", ctx)
 			fmt.Println("Goroutines after Game end\t", runtime.NumGoroutine())
 
+			//Was just testing Channels between /tty and websockets 
 			c := fanIn(boring("Joe"), boring("Ann"))
 			for i := 0; i < 10; i++ {
 				fmt.Println(<-c)
@@ -200,6 +207,7 @@ func tty(w http.ResponseWriter, req *http.Request) {
 	}*/
 }
 
+//Not using this function, but kept it from the early tests of GoTTY.
 func exit(err error, code int) {
 	if err != nil {
 		fmt.Println(err)
@@ -238,6 +246,8 @@ func socketshandle(w http.ResponseWriter, r *http.Request) {
 }
 
 
+
+//Gorilla websockets which if establishes just keeps sending user JSON data to browser every 5 seconds untile ws.close().
 func v3ws(w http.ResponseWriter, r *http.Request) {
 	var conn, _ = upgrader.Upgrade(w, r, nil)
 	go func(conn *websocket.Conn) {
@@ -254,6 +264,7 @@ func v3ws(w http.ResponseWriter, r *http.Request) {
 }
 
 
+//Testing with example of HandlerFunc from Golang Web Dev course
 type hotcat int
 
 func (c hotcat) ServeHTTP(res http.ResponseWriter, req *http.Request) {
