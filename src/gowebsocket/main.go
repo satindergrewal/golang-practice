59a9a287ae2a59b041d65432a186382437cc24fa
Satinder Grewal <grewal@lavabit.com>
2019-03-06 00:42:14 +1300

    go official websockets example

---
+++ src/gowebsocket/main.go
@@ -0,0 +1,40 @@
+// https://astaxie.gitbooks.io/build-web-application-with-golang/en/08.2.html
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
+	"net/http"
+)
+
+func Echo(ws *websocket.Conn) {
+	var err error
+
+	for {
+		var reply string
+
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+
+		fmt.Println("Received back from client: " + reply)
+
+		msg := "Received:  " + reply
+		fmt.Println("Sending to client: " + msg)
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
+func main() {
+	http.Handle("/", websocket.Handler(Echo))
+
+	if err := http.ListenAndServe(":1234", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
