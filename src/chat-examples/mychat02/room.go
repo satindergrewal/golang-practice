64b9e4d57a7432bd0ca789d45bcfbf78396cbcb5
satindergrewal <grewal@lavabit.com>
2019-03-19 17:00:58 +1300

    run method added to room.go

--- src/chat-examples/mychat02/room.go
+++ src/chat-examples/mychat02/room.go
@@ -4,4 +4,36 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
 	forward chan []byte
+	// join is channel for clients wishing to join the room.
+	join chan *client
+	// leave is a channel for clients wishing to leave the room.
+	leave chan *client
+	// clients holds all current clients in this room.
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward messages to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send the message
+				default:
+					// failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
