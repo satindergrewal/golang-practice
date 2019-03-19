e6123b8b59a44a193220fb9a12cb05e2a4c3874e
satindergrewal <grewal@lavabit.com>
2019-03-19 18:06:59 +1300

    newRoom method added, room handler added to main, html updated with input elements

--- src/chat-examples/mychat02/main.go
+++ src/chat-examples/mychat02/main.go
@@ -24,7 +24,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 
 	//  Start the Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
