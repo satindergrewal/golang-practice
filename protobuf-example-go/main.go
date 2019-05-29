896af0618749f708142bb60a29a2198537666a51
satindergrewal <grewal@lavabit.com>
2019-05-29 23:05:15 +1200

    simple protobuf example with go

--- protobuf-example-go/main.go
+++ protobuf-example-go/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	simplepb "golang-practice/protobuf-example-go/src/simple"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	doSimple()
+}
+
+func doSimple() {
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My Simple Message",
+		SampleList: []int32{1, 4, 7, 8},
+	}
+
+	fmt.Println(sm)
+
+	sm.Name = "I renamed you"
+
+	fmt.Println(sm)
+
+	// Always prefer to use helper funcs for such Id info instead of directly using sm.Id
+	fmt.Println("The ID is: ", sm.GetId())
 }
