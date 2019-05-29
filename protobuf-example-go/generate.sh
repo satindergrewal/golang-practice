31c2d277acb57a9ffacd3d0dd7a6bbf745a2cef8
satindergrewal <grewal@lavabit.com>
2019-05-30 02:05:00 +1200

    addressbookpb

--- protobuf-example-go/generate.sh
+++ protobuf-example-go/generate.sh
@@ -1,3 +1,6 @@
 protoc -I src/ --go_out=src/ src/simple/simple.proto
 protoc -I src/ --go_out=src/ src/enum_example/enum_example.proto
 protoc -I src/ --go_out=src/ src/complex/complex.proto
+
+# Practice proto AddressBook compile command
+protoc -I src/ --go_out=src/ src/AddressBook/addressbook.proto
