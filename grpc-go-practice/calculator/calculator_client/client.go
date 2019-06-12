2e3ca96a8217567615e77143af9dfbfe2a6bbdb4
Satinder Grewal <grewal@lavabit.com>
2019-06-13 00:03:22 +1200

    protobuf calculator Sum gRPC server client practice

---
+++ grpc-go-practice/calculator/calculator_client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang-practice/grpc-go-practice/calculator/calculatorpb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Calculator Client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+	//fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do Sum Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  3,
+		SecondNumber: 10,
+	}
+
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
+	}
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
---
+++ grpc-go-practice/calculator/calculator_server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"golang-practice/grpc-go-practice/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	log.Printf("Recieved Sum RPC: %v\n", req)
+	firstNumber := req.FirstNumber
+	secondNumber := req.SecondNumber
+	sum := firstNumber + secondNumber
+	res := &calculatorpb.SumResponse{
+		SumResult: sum,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Calculator Srver")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
